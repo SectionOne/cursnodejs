b60153c4b742e42258ac7a68b7c7c20a61e12c62
Section1 <otinoco@digiocio.com>
2022-09-14 03:33:17 +0200

    CODE 35 - Afegint el ús de Middleware en les rutes

--- routes/usuari.js
+++ routes/usuari.js
@@ -5,13 +5,14 @@ var UsuariController = require("../controllers/usuari");
 
 var api = express.Router();
 var multipart = require("connect-multiparty");
+var md_auth = require("../middleware/authenticated");
 
 //middleware
 var md_upload = multipart({ uploadDir: './uploads/users'});
 
 api.get("/tots", UsuariController.proves);
 api.post("/registre", UsuariController.guardarUsuari);
-api.post("/veureusuari", UsuariController.veureUsuari);
+api.post("/veureusuari", md_auth.ensureAuth, UsuariController.veureUsuari); //Securitzem la ruta, obligant a passar per el middleware abans de accionar el controlador
 api.get("/veuretotsusuari", UsuariController.veureTotsUsuari);
 api.put("/actualitzar-usuari/:id", UsuariController.actualitzarUsuari);
 api.delete("/borrarusuari/:id", UsuariController.borrarUsuari);
